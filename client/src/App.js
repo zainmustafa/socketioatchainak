dc55608e62aa33515a03250dab853b947395871c
zainmustafa <zmdhillo@gmail.com>
2017-10-07 00:38:18 +0500

    integrate react app with express

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {users: []};
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Socet.IO at Chainak</h1>
+                </header>
+                <h1>Users</h1>
+                {this.state.users.map(user =>
+                    <div key={user.id}>{user.username}</div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
